a10f1f375031c22db3989cfc66616634859d9e00
emperifybusiness <149296329+emperifybusiness@users.noreply.github.com>
2024-03-11 16:22:44 +0530

    changes

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -8,24 +8,25 @@ const Chatbot = () => {
     document.body.appendChild(script)
  
     script.onload = () => {
-        window.botpressWebChat.init({
-            "composerPlaceholder": "Ask us anything!",
-            // "botConversationDescription": "This chatbot was built surprisingly fast with Botpress",
-            "botId": "bd1cc78e-36ea-48bc-9397-00b525074155",
-            "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
-            "messagingUrl": "https://messaging.botpress.cloud",
-            "clientId": "bd1cc78e-36ea-48bc-9397-00b525074155",
-            "webhookId": "e9cb8cff-8290-4039-8556-eda7ddd0e047",
-            "lazySocket": true,
-            "themeName": "prism",
-            "botName": "Goa Destination Weddiings",
-            "avatarUrl": "https://i.ibb.co/ryxHKKM/Happy-Women-s-Day-Instagram-Post-2.png",
-            "stylesheet": "https://webchat-styler-css.botpress.app/prod/6e72f92d-51bf-4e80-9753-d964a044a755/v67634/style.css",
-            "frontendVersion": "v1",
-            "useSessionStorage": true,
-            "theme": "prism",
-            "themeColor": "#2563eb"
-        });
+      window.botpressWebChat.init({
+        "composerPlaceholder": "say hi!",
+        "botConversationDescription": "your assistant",
+        "botId": "81fc9202-c902-4581-ab65-e9650213b002",
+        "avatarUrl": "https://i.ibb.co/LYfmySF/Goadestination-Weddzing-7.png",
+        "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+        "messagingUrl": "https://messaging.botpress.cloud",
+        "clientId": "81fc9202-c902-4581-ab65-e9650213b002",
+        "webhookId": "1a244e21-1335-4c39-a63b-41a366f76415",
+        "lazySocket": true,
+        "themeName": "prism",
+        "botName": "Goa Destination Weddings",
+        "stylesheet": "https://webchat-styler-css.botpress.app/prod/6e72f92d-51bf-4e80-9753-d964a044a755/v29842/style.css",
+        "frontendVersion": "v1",
+        "useSessionStorage": true,
+        "enableConversationDeletion": true,
+        "theme": "prism",
+        "themeColor": "#2563eb"
+    });
     }
   }, [])
  
